3d1bc0cae385d576c12c9b1d8cdd4e7b10107364
PV - Infnet <pv-infnet@MBANK-G50DXX0VCK.local>
2024-11-24 08:44:20 -0300

    configurando tablet

--- app/listMarkers.tsx
+++ app/listMarkers.tsx
@@ -1,27 +1,27 @@
-import AppBarComponent from '@/components/appBarComponent';
-import MarkerComponent from '@/components/markerComponent';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { router } from 'expo-router';
-import { useEffect, useState } from 'react';
-import { FlatList, View } from 'react-native'
+import AppBarComponent from '@/components/appBarComponent'
+import MarkerComponent from '@/components/markerComponent'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { router } from 'expo-router'
+import { useEffect, useState } from 'react'
+import { FlatList, View, StyleSheet } from 'react-native'
 
 export default function ListMarkers() {
 
-    const [markersList, setMarkersList] = useState<Array<any>>([]);
+    const [markersList, setMarkersList] = useState<Array<any>>([])
 
     useEffect(() => {
         (async () => {
-            const markersStorage = await AsyncStorage.getItem('markers');
+            const markersStorage = await AsyncStorage.getItem('markers')
             if (markersStorage) {
-                const parsedMarkers = JSON.parse(markersStorage);
-                setMarkersList(parsedMarkers);
+                const parsedMarkers = JSON.parse(markersStorage)
+                setMarkersList(parsedMarkers)
             }
-        })();
-    }, []);
+        })()
+    }, [])
 
     const markerPress = (index: number) => {
         router.push(`/editMarker?index=${index}`)
-    };
+    }
 
     return (
         <View>
@@ -37,8 +37,20 @@ export default function ListMarkers() {
                         onPress={() => markerPress(index)}
                         nome={item.nome}
                         latLng={item.latLng}
-                        cor={item.cor} />
-                )} />
+                        cor={item.cor}
+                    />
+                )}
+                contentContainerStyle={styles.contentContainer}
+            />
         </View>
-    );
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    contentContainer: {
+        paddingBottom: 40, 
+    },
+})
